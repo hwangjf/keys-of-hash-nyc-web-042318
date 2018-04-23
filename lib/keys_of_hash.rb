c3200be51d323a331dbf5bbfac2a785dc4731023
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-23 11:27:16 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |args|
+      array << self.key(args)
+    end
+    return array
   end
 end
